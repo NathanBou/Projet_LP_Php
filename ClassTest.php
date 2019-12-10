76a0e51a1d007d3d0b283deb3b959dc3e5a6aeea
bouviern <nath.bou38@gmail.com>
2019-12-10 09:57:47 +0100

    Test commit

--- ClassTest.php
+++ ClassTest.php
@@ -3,5 +3,15 @@
 
 class ClassTest
 {
-    
+
+    public string $tot;
+    /**
+     * ClassTest constructor.
+     */
+    public function __construct(string $toto)
+    {
+        $this->tot = $toto;
+    }
+
+
 }
